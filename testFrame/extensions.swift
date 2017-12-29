26993920b27e5e15af18f42295a4c1b8502f5c54
xianhuanlin <xianhuanlin@xianhuanlindemacbook-pro-2.local>
2017-12-29 10:57:08 +0800

    修改一下

--- testFrame/extensions.swift
+++ testFrame/extensions.swift
@@ -16,3 +16,154 @@ extension NSObject{
         }
     }
 }
+
+extension UIView{
+    public var height: CGFloat{
+        get{
+            return self.frame.size.height
+        }
+        set{
+            var r = self.frame
+            r.size.height = newValue
+            self.frame = r
+        }
+    }
+
+    public var width: CGFloat{
+        get{
+            return self.frame.size.width
+        }
+        set{
+            var r = self.frame
+            r.size.width = newValue
+            self.frame = r
+        }
+    }
+
+    public var top: CGFloat{
+        get{
+            return self.frame.origin.y;
+        }
+        set{
+            var r = self.frame
+            r.origin.y = newValue
+            self.frame = r
+        }
+    }
+
+    public var left: CGFloat{
+        get{
+            return self.frame.origin.x;
+        }
+        set{
+            var r = self.frame
+            r.origin.x = newValue
+            self.frame = r
+        }
+    }
+
+    public var bottom: CGFloat{
+        get{
+            return self.frame.origin.y + self.frame.size.height;
+        }
+        set{
+            var r = self.frame
+            r.origin.y = newValue - self.frame.size.height
+            self.frame = r
+        }
+    }
+
+    public var right: CGFloat{
+        get{
+            return self.frame.origin.x + self.frame.size.width;
+        }
+        set{
+            var r = self.frame
+            r.origin.x = newValue - self.frame.size.width
+            self.frame = r
+        }
+    }
+
+    public var centerH: CGFloat{
+        get{
+            return self.center.x
+        }
+        set{
+            var p = self.center
+            p.x = newValue
+            self.center = p
+        }
+    }
+
+    public var centerV: CGFloat{
+        get{
+            return self.center.y
+        }
+        set{
+            var p = self.center
+            p.y = newValue
+            self.center = p
+        }
+    }
+    
+    public var cornerRadius:CGFloat{
+        get{
+            return self.layer.cornerRadius
+        }
+        set{
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    public var borderColor:UIColor?{
+        get{
+            return UIColor.init(cgColor: self.layer.borderColor ?? UIColor.clear.cgColor)
+        }
+        set{
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    public var borderWidth:CGFloat{
+        get{
+            return self.layer.borderWidth
+        }
+        set{
+            self.layer.borderWidth = newValue
+        }
+    }
+}
+
+extension CGFloat{
+    func f()->Float{
+        return Float(self)
+    }
+}
+
+extension Float{
+    func cf()->CGFloat{
+        return CGFloat(self)
+    }
+}
+
+extension Double{
+    func cf()->CGFloat{
+        return CGFloat(self)
+    }
+}
+
+extension Int{
+    func f()->Float{
+        return Float(self)
+    }
+    
+    func cf()->CGFloat{
+        return CGFloat(self)
+    }
+    
+    func d()->Double{
+        return Double(self)
+    }
+}
+
+
