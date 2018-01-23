5a0e3a712b2bb99e2e472b8e8ef8ddeae20005e4
xianhuanlin <linxianhuan@126.com>
2018-01-23 14:59:51 +0800

    add test code

--- host/src/detail.vue
+++ host/src/detail.vue
@@ -1,36 +1,109 @@
 <template>
     <scroller class="root">
         <div v-if="" class="mainCell">
-            <image class="mainImage" :src="imageSet[0].image_url"></image>
+            <image class="mainImage" :src="imageSet[0].image_url" @click="mainImageClick"></image>
             <text v-if="salePoint.length>0" class="salePoint">{{salePoint}}</text>
             <text class="title">{{title}}</text>
 
             <div class="divPrice">
-                <text class="price">1</text>
-                <text class="price2">2</text>
-                <text class="marketPrice">3</text>
+                <text class="price">￥</text>
+                <text class="price2">{{price}}</text>
+                <text class="marketPrice">{{marketPrice}}</text>
             </div>
+
             <div  class="divDeliver">
                 <div class="divDeliverCell" v-for="num in deliverInfo">
                     <image src="asset://icon-detailpage-OK" class="deliverIcon"></image>
                     <text class="deliver">{{num}}</text>
                 </div>
-                <!--<div class="divDeliverCell">-->
-                    <!--<image src="asset://icon-detailpage-OK" class="deliverIcon"></image>-->
-                    <!--<text class="deliver">item</text>-->
-                <!--</div>-->
-                <!--<div class="divDeliverCell">-->
-                    <!--<image src="asset://icon-detailpage-OK" class="deliverIcon"></image>-->
-                    <!--<text class="deliver">item</text>-->
-                <!--</div>-->
             </div>
+        </div>
+        <div class="activityCell" v-for="num in activityList">
+            <div class="activityItem">
+                <image></image>
+                <text></text>
+                <text></text>
+                <image></image>
+            </div>
+        </div>
+        <div class="rulecell">
+            <image class="ruleImage" :src="ruleImage"></image>
+        </div>
+        <div class="sunbianCell">
+            <text class="sunbianTitle">笋编说</text>
+            <text class="sunbianInfo">{{sunbianInfo}}</text>
+        </div>
+        <div class="brandCell">
+            <text class="brandtitle">品牌</text>
+            <image class="brandimage" :src="brandData.image"></image>
+            <text class="brandname">{{brandData.name}}</text>
+            <text class="brandinfo">{{brandData.info}}</text>
 
         </div>
-        <web class="webView" :src="detailUrl"></web>
+        <!--<div>-->
+            <web2 class="webView" :src="detailUrl"></web2>
+        <!--</div>-->
+
     </scroller>
 </template>
 
 <style scoped>
+    .sunbianCell{
+        padding: 30px;
+        background-color: white;
+        margin-top: 20px;
+    }
+    .sunbianTitle{
+        font-size: 30px;
+        color: #4A4A4A;;
+    }
+    .sunbianInfo{
+        font-size: 26px;
+        color: #919191;;
+        margin-top: 10px;
+        /*lines:2;*/
+    }
+    .brandCell{
+        padding: 30px;
+        margin-top: 20px;
+        background-color: white;
+        height: 288px;
+    }
+    .brandtitle{
+        font-size: 26px;
+        color: #4a4a4a;
+    }
+    .brandimage{
+        width: 170px;
+        height: 170px;
+        margin-top: 30px;
+        border-width: 2px;
+        border-color: #ebe9e9;
+        position: absolute;
+    }
+    .brandname{
+        margin-top: 30px;
+        margin-left: 200px;
+        font-size: 30px;
+        font-weight: bold;
+        color: #4a4a4a;
+        position: absolute;
+    }
+    .brandinfo{
+        margin-top: 80px;
+        margin-left: 200px;
+        font-size: 26px;
+        color: #4a4a4a;
+        position: absolute;
+        lines:2;
+    }
+
+    .activityItem{
+
+    }
+    .activityCell{
+
+    }
     .deliver{
         /*width: auto;*/
         /*height: 30px;*/
@@ -46,6 +119,7 @@
     }
 
     .divDeliver{
+        margin-top: 10px;
         height: 74px;
         flex-direction: row;
         padding-left: 30px;
@@ -63,17 +137,14 @@
         width: 750px;
         height: 1750px;
         margin-top: 20px;
+        /*position: relative;*/
     }
 
     .mainCell{
         background-color: white;
         flex-direction: column;
     }
-    .divPrice{
-        /*margin-top: ;*/
-        flex-direction: row;
-        /*justify-content: space-between;*/
-    }
+
     .mainImage{
         width: 750px;
         height: 750px;
@@ -92,25 +163,35 @@
         padding-left: 30px;
         padding-right: 30px;
     }
+    .divPrice{
+        margin-top: 10px;
+        flex-direction: row;
+        align-items: flex-end;
+        padding-left: 30px;
+        padding-right: 30px;
+        /*justify-content: space-between;*/
+    }
     .price{
         font-size: 26px;
         color: #4A4A4A;
-        margin-top: 10px;
-        padding-left: 30px;
-        padding-right: 30px;
+        /*margin-top: 10px;*/
+        /*padding-left: 30px;*/
+        /*padding-right: 30px;*/
     }
     .price2{
         font-weight: bold;
         font-size: 40px;
-        margin-top: 10px;
-        padding-left: 30px;
-        padding-right: 30px;
-        color: #DAD8D8;
+        /*margin-top: 10px;*/
+        /*padding-left: 30px;*/
+        /*padding-right: 30px;*/
+        color: #4A4A4A;
+        margin-left: 5px;
     }
     .marketPrice{
         font-size: 26px;
         color: #DAD8D8;
         text-decoration:line-through;
+        margin-left: 10px;
     }
     .
     .deliver{
@@ -129,15 +210,17 @@
                 imageSet:[{image_url:''}],
                 deliverInfo:['七天包换','七天包换','正品保证'],
 
-
                 title: "card info",
                 salePoint:"好卖",
 
                 price:"123",
-                marketPrice:"112",
+                marketPrice:"￥130",
                 detailUrl:"https://h5.watsons.com.cn/appItem?url=http://asset.watsons.com.cn/d/1286_04a349f7a0b8600b7b60cfbccb5a40ac.html",
                 loadingOk:false,
-
+                sunbianInfo:'',
+                ruleImage:'',
+                activityList:[],
+                brandData:{image:'',info:'',},
 
             }
         },
@@ -166,12 +249,20 @@
                 ws.imageSet = item.item_sku_image_list
                 wtsEvent.toast(item.item_long_name);
                 console.log(rsp);
+
+                ws.marketPrice = weex.config.bundleUrl
+                ws.sunbianInfo = '我是一个笋编说'
+                ws.brandData= {image:item.item_brand.logo,info:item.item_long_name + item.item_long_name,id:item.item_brand.id,name:item.item_brand.brand_name,}
+
             })
         },
         methods: {
             update: function (e) {
                 this.title = "click" + this.title
 
+            },
+            mainImageClick:function (e) {
+                wtsEvent.showFullImage([this.imageSet[0].image_url],0)
             }
         }
     }
